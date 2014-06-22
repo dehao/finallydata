17646ae6aec245d17d6b23815a7a421cc8b1babf
dehao <linkin3254317@gmail.com>
2014-06-23 00:24:10 +0800

    sillouhoute

--- ckip/silhouette.py
+++ ckip/silhouette.py
@@ -1,12 +1,12 @@
 import Orange
 
-voting = Orange.data.Table("reconstructedterm.csv")
+voting = Orange.data.Table("ckip_ed_all_reducedu.csv")
 # table = Orange.data.Table("iris")
+print "Doneread:"
+#for k in range(357, 377):
+#    km = Orange.clustering.kmeans.Clustering(voting, k, initialization=Orange.clustering.kmeans.init_diversity, distance=Orange.distance.PearsonR)
+#    score = Orange.clustering.kmeans.score_fast_silhouette(km)
+#    print k, score
 
-#for k in range(2, 5):
-    #km = Orange.clustering.kmeans.Clustering(voting, k, initialization=Orange.clustering.kmeans.init_diversity)
-    #score = Orange.clustering.kmeans.score_silhouette(km)
-    #print k, score
-
-km = Orange.clustering.kmeans.Clustering(voting, 20, initialization=Orange.clustering.kmeans.init_diversity)
+km = Orange.clustering.kmeans.Clustering(voting, 10, initialization=Orange.clustering.kmeans.init_diversity, distance=Orange.distance.PearsonR)
 Orange.clustering.kmeans.plot_silhouette(km, "kmeans-silhouette.png")
